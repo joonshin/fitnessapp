22ed771f8d885277cc6d76a2b55fb73030a2783d
joonshin <jshin170@gmail.com>
2017-04-26 14:39:53 -0400

    Basic Auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Fitness App 
                 </div>
 
                 <div class="links">
